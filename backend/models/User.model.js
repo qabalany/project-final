3fb2b72ec6b4f565bb8162c5e83441a13da05d03
Mohammad Qabalany <qabalany@gmail.com>
2026-02-23 23:19:56 +0100

    add password hashing logic to user model

--- backend/models/User.model.js
+++ backend/models/User.model.js
@@ -1,5 +1,6 @@
 // Okay, this is where I define what a "User" looks like in my database
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema(
     {
@@ -49,6 +50,29 @@ const userSchema = new mongoose.Schema(
         timestamps: true, // Mongoose will automatically add createdAt and updatedAt dates for me!
     }
 );
+
+// --- Password Protection Magic ---
+
+// 1. Before saving a user to the database, I need to scramble their password
+userSchema.pre('save', async function (next) {
+    // If the user isn't updating their password, skip this step so I don't double-hash it!
+    if (!this.isModified('password')) {
+        return next();
+    }
+
+    // Generate a random "salt" (extra characters) to make the hash even stronger
+    const salt = await bcrypt.genSalt(10);
+
+    // Replace the plain text password with the scrambled hash
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+// 2. Later, when the user logs in, I need a way to check if their entered password matches the scrambled one in the database
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    // bcrypt handles the math to see if "password123" matches the crazy string in the database
+    return await bcrypt.compare(enteredPassword, this.password);
+};
 
 // I create the model and export it so I can use it in my routes later to find, create, or update users
 export default mongoose.model('User', userSchema);
